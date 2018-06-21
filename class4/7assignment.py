e36c91f780887930c1fd292e1bd780ea45e5ce43
Harmandhindsa19 <40339591+Harmandhindsa19@users.noreply.github.com>
2018-06-21 14:36:48 +0530

    Add files via upload

---
+++ class4/7assignment.py
@@ -0,0 +1,14 @@
+q="mississippi"
+print(q)
+a=list(q)
+print(a)
+k=a.count("m")
+print("m present in keyword:" , k)
+q=a.count("i")
+print("q present in keyword:" , q)
+r=a.count("s")
+print("s present in keyword:" , r)
+s=a.count("p")
+print("p present in keyword:" , s)
+dic={"m":k,"i":q,"s":r,"p":s}
+print(dic)
