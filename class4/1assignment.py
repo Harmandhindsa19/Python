68dfb7db1ce5d0604dc2d24348110a8010cc4e5b
Your Name <you@example.com>
2018-06-21 11:12:59 +0530

    2 questions are done

---
+++ class4/1assignment.py
@@ -0,0 +1,3 @@
+t=(1,2,3,4,5.5,7.8,2,"a","xyz")
+print(t)
+print("length of tuple is : %d " %len(t))
---
+++ class4/2assignment.py
@@ -0,0 +1,4 @@
+t=(20,50,30,60,78,19,49)
+print("tuple is:" , t)
+print("maximun number in tuple is:" , max(t))
+print("minimum number in tuple is:" , min(t))
