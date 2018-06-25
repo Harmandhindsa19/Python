4c389ffc1a7d32bd4f830a793eacb3ba56162a26
Your Name <you@example.com>
2018-06-25 14:59:34 +0530

    class 12 assignment 1 commited

---
+++ class12/1assignment.py
@@ -0,0 +1,13 @@
+#access token
+An access token is an object that describes the security context of a process or thread. The information in a token includes the identity and privileges of the user account associated with the process.async
+#to generate access token-
+1.firstly we have to open the website - www.apps.twitter.com
+2.login your twitter account onto that website.
+3.after logging in , fill up the application form.
+4.by accept all the terms and conditions , cosumer id and secret will create.
+5.by clicking on button get ur access token , user will get his/her access token , access token secret.
+
+
+
+
+
