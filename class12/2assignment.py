cbdc964ecc03f2c5022fb94aad84ae7d83649a27
Your Name <you@example.com>
2018-06-25 15:00:44 +0530

    class 12 assignment 2 commited

---
+++ class12/2assignment.py
@@ -0,0 +1,25 @@
+#ip address for google
+(venv) C:\Users\Muneet\PycharmProjects\class12>nslookup www.google.com
+Server:  UnKnown
+Address:  192.168.43.1
+
+Non-authoritative answer:
+Name:    www.google.com
+Addresses:  2404:6800:4009:80e::2004
+          172.217.160.196
+#ip address for instagram
+(venv) C:\Users\Muneet\PycharmProjects\class12>nslookup www.instagram.com
+Server:  UnKnown
+Address:  192.168.43.1
+
+Non-authoritative answer:
+Name:    z-p15-instagram.c10r.facebook.com
+Addresses:  2a03:2880:f12f:85:face:b00c:0:61e8
+          157.240.16.37
+Aliases:  www.instagram.com
+
+
+(venv) C:\Users\Muneet\PycharmProjects\class12>
+
+
+
