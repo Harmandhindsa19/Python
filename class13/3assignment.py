7b17b3e8fedd98a1ef97f2097c16a0fd769ea0c1
Your Name <you@example.com>
2018-06-26 14:23:20 +0530

    question 3 is done

---
+++ class13/3assignment.py
@@ -0,0 +1,9 @@
+# Program to depict Raising Exception
+# class NameError(Exception):
+#     pass
+try:
+    raise NameError("Hi there")  # Raise Error
+except NameError:
+    print("An exception")
+    raise  # To determine whether the exception was raised or not
+
